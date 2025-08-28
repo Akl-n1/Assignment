9b5f663bd60f2180f70237652daba9b9d7176746
Akl-n1 <ma7mud3kl@gmail.com>
2025-08-28 23:56:00 +0300

    solve Task10

---
+++ Day10_c#/Day10_c#/Day10_c#/AllClasses.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_c_
+{
+
+    #region Main Employee
+    //class Employee : IComparable<Employee>
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public int Salary { get; set; }
+
+    //    //public int CompareTo(Employee? other)
+    //    //{
+    //    //    if (other == null) return 1;
+    //    //    return this.Id.CompareTo(other.Id);
+    //    //}
+    //    public int CompareTo(Employee? other)
+    //    {
+    //        if (other == null) return 1;
+    //        int result = this.Salary.CompareTo(other.Salary);
+    //        if (result == 0)
+    //            return this.Name.CompareTo(other.Name);
+    //        return result;
+    //    }
+    //    public override string ToString()
+    //    {
+    //        return $"{Id} - {Name} - Salary: {Salary}";
+    //    }
+    //}
+    #endregion
+
+    class Employee : ICloneable, IComparable<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+        // Implement Clone
+        public object Clone()
+        {
+            return new Employee
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Salary = this.Salary
+            };
+        }
+
+        public int CompareTo(Employee other)
+        {
+            if (other == null) return 1;
+            int result = this.Salary.CompareTo(other.Salary);
+            if (result == 0)
+                result = this.Name.CompareTo(other.Name);
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"Id={Id}, Name={Name}, Salary={Salary}";
+        }
+    }
+
+    #region p1
+    //class SortingAlgorithm1<T> where T : Employee
+    //{
+    //    public static void Sort(T[] Arr)
+    //    {
+
+    //        for (int i = 0; i < Arr.Length - 1; i++)
+    //        {
+    //            int min = i;
+    //            for (int j = i + 1; j < Arr.Length; j++)
+    //            {
+    //                if (Arr[j].CompareTo(Arr[min]) < 0)
+    //                    min = j;
+    //            }
+    //            T Temp = Arr[min];
+    //            Arr[min] = Arr[i];
+    //            Arr[i] = Temp;
+
+    //        }
+
+    //    }
+    //}
+
+    #endregion
+
+    #region P2
+
+    //class SortingAlgorithm2
+    //{
+    //    public static void Sort<T>(T[] Arr, Func<T, T, int> compare)
+    //    {
+
+    //        for (int i = 0; i < Arr.Length - 1; i++)
+    //        {
+    //            int min = i;
+    //            for (int j = i + 1; j < Arr.Length; j++)
+    //            {
+    //                if (compare(Arr[j], (Arr[min])) < 0)
+    //                    min = j;
+    //            }
+    //            T Temp = Arr[min];
+    //            Arr[min] = Arr[i];
+    //            Arr[i] = Temp;
+
+    //        }
+
+    //    }
+    //} 
+    #endregion
+
+    #region P3
+    //class SortingAlgorithm3
+    //{
+    //    public static void Sort<T>(T[] Arr, Func<T, T, int> compare)
+    //    {
+
+    //        for (int i = 0; i < Arr.Length - 1; i++)
+    //        {
+    //            int min = i;
+    //            for (int j = i + 1; j < Arr.Length; j++)
+    //            {
+    //                if (compare(Arr[j], (Arr[min])) < 0)
+    //                    min = j;
+    //            }
+    //            T Temp = Arr[min];
+    //            Arr[min] = Arr[i];
+    //            Arr[i] = Temp;
+
+    //        }
+
+    //    }
+    //} 
+    #endregion
+
+    #region P4
+
+    //class Manager : Employee, IComparable<Manager>
+    //{
+    //    public int CompareTo(Manager? other)
+    //    {
+    //        if (other == null) return 1;
+    //        return this.Salary.CompareTo(other.Salary);
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"Manager: {Name}, Salary: {Salary}";
+    //    }
+    //}
+    //class SortingAlgorithm4
+    //{
+    //    public static void Sort<T>(T[] Arr, Func<T, T, int> Compare)
+    //    {
+    //        for (int i = 0; i < Arr.Length - 1; i++)
+    //        {
+    //            int min = i;
+    //            for (int j = i + 1; j < Arr.Length; j++)
+    //            {
+    //                if (Compare(Arr[j], Arr[min]) < 0)
+    //                {
+    //                    min = j;
+    //                }
+    //            }
+    //            T temp = Arr[min];
+    //            Arr[min] = Arr[i];
+    //            Arr[i] = temp;
+    //        }
+
+    //    }
+    //} 
+    #endregion
+
+
+    class SortingAlgorithm5
+    {
+        public static void Sort<T>(T[] Arr, Func<T, T, bool> Compare)
+        {
+            for (int i = 0; i < Arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < Arr.Length; j++)
+                {
+                    if (!Compare(Arr[i], Arr[j]))
+                    {
+                        T temp = Arr[i];
+                        Arr[i] = Arr[j];
+                        Arr[j] = temp;
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    class SortingAlgorithm6
+    {
+        public static void Sort<T>(T[] Arr, Func<T, T, bool> Compare)
+        {
+            for (int i = 0; i < Arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < Arr.Length; j++)
+                {
+                    if (!Compare(Arr[i], Arr[j]))
+                    {
+                        T temp = Arr[i];
+                        Arr[i] = Arr[j];
+                        Arr[j] = temp;
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    class SortingAlgorithm7
+    {
+        public static void Swap<U>(ref U num1, ref U num2)
+        {
+            U temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        public static void Sort<U>(U[] Arr) where U : IComparable<U>
+        {
+            for (int i = 0; i < Arr.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < Arr.Length; j++)
+                {
+                    if (Arr[j].CompareTo(Arr[min]) < 0)
+                    {
+                        min = j;
+                    }
+                }
+                Swap(ref Arr[i], ref Arr[min]);
+            }
+
+        }
+    }
+
+    class SortingAlgorithm8
+    {
+        public static void Sort<T>(T[] arr) where T : IComparable<T>
+        {
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    int min = i;
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[j].CompareTo(arr[min]) < 0)
+                        {
+                            T temp = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class SortingAlgorithm<T> where T : ICloneable, IComparable<T>
+    {
+        public static T[] Sort(T[] arr)
+        {
+            T[] CloneArr = new T[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                CloneArr[i] = (T)arr[i].Clone();
+            }
+            for (int i = 0; i < CloneArr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < CloneArr.Length; j++)
+                {
+                    if (CloneArr[j].CompareTo(CloneArr[i]) < 0)
+                    {
+                        T temp = CloneArr[j];
+                        CloneArr[j] = CloneArr[i];
+                        CloneArr[i] = temp;
+                    }
+                }
+            }
+            return CloneArr;
+        }
+    }
+
+
+    class Transformation
+    {
+        public delegate string StringTransformer(string input);
+        public static List<string> ApplyTransformation(List<string> input, StringTransformer stringTransformer)
+        {
+            List<string> output = new List<string>();
+            foreach (string inputItem in input)
+            {
+                output.Add(stringTransformer(inputItem));
+            }
+            return output;
+        }
+    }
+
+    class operations
+    {
+        public delegate int Operations(int num1, int num2);
+        public static int Calculate(int num1, int num2, Operations op)
+        {
+            return op(num1, num2);
+        }
+    }
+
+    public class TransformList
+    {
+        public delegate R Transform<T, R>(T list);
+        public static List<R> TransformListMethos<T, R>(List<T> list, Transform<T, R> transform)
+        {
+            List<R> Result = new List<R>();
+            foreach (T item in list)
+            {
+                Result.Add(transform(item));
+            }
+            return Result;
+
+        }
+
+        public static List<TResult> Squre<T, TResult>(List<T> values, Func<T, TResult> squre)
+        {
+            var result = new List<TResult>();
+            foreach (T item in values)
+                result.Add(squre(item));
+            return result;
+        }
+
+        public static List<T> Filter<T>(List<T> values, Predicate<T> condition)
+        {
+            var Result = new List<T>();
+            foreach (var item in values)
+                if (condition(item))
+                    Result.Add(item);
+            return Result;
+        }
+
+        public static List<string> FilterStrings(List<string> list, Predicate<string> condition)
+        {
+            List<string> result = new List<string>();
+            foreach (var str in list)
+            {
+                if (condition(str)) // الشرط اللى اتبعت
+                {
+                    result.Add(str);
+                }
+            }
+            return result;
+        }
+
+        public static int PerformOperation(int a, int b, Func<int, int, int> operation)
+        {
+            return operation(a, b);
+        }
+
+        public static double PerformOperation(double a, double b, Func<double, double, double> operation)
+        {
+            return operation(a, b);
+        }
+    }
+
+
+}
+
---
+++ Day10_c#/Day10_c#/Day10_c#/Program.cs
@@ -0,0 +1,249 @@
+﻿
+
+
+
+namespace Day10_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem 1
+
+            //     Employee[] employees =
+            //{
+            //               new Employee { Id = 1, Name = "Ali", Salary = 5000 },
+            //               new Employee { Id = 2, Name = "Mona", Salary = 3000 },
+            //               new Employee { Id = 3, Name = "Omar", Salary = 7000 },
+            //               new Employee { Id = 4, Name = "Sara", Salary = 4000 }
+            //           };
+            //     SortingAlgorithm1<Employee>.Sort(employees);
+            //     foreach (var employee in employees)
+            //     {
+            //         Console.WriteLine(employee);
+            //     }
+
+            #endregion
+
+            #region Problem 2
+            //int[] numbers = { 6, 3, 5, 2, 7 };
+
+            //SortingAlgorithm2.Sort(numbers, (a, b) => b.CompareTo(a));
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //} 
+            #endregion
+
+            #region Problem 3
+            //string[] numbers = { "Ali", "Mahmoud", "Akl", "Sara" };
+
+            //SortingAlgorithm2.Sort(numbers, (a, b) => b.Length.CompareTo(a.Length));
+
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+
+            //} 
+            #endregion
+
+            #region Problem 4
+            //    Manager[] managers =
+            //  {
+            //    new Manager { Id = 1, Name = "Ahmed", Salary = 9000 },
+            //    new Manager { Id = 2, Name = "Mona", Salary = 12000 },
+            //    new Manager { Id = 3, Name = "Omar", Salary = 8000 },
+            //    new Manager { Id = 4, Name = "Omar", Salary = 7000 },
+            //    new Manager { Id = 5, Name = "Omar", Salary = 10000 }
+            //};
+
+            //    Console.WriteLine("Before Sorting:");
+            //    foreach (var m in managers) Console.WriteLine(m);
+
+            //    SortingAlgorithm4.Sort(managers, (a, b) => b.CompareTo(a));
+
+            //    Console.WriteLine("\nAfter Sorting by Salary (Ascending):");
+            //    foreach (var m in managers) Console.WriteLine(m);
+
+            #endregion
+
+            #region Problem 5
+            // Employee[] employees =
+            //  {
+            // new Employee { Id = 1, Name = "Ahmed", Salary = 9000 },
+            // new Employee { Id = 2, Name = "Mona", Salary = 12000 },
+            // new Employee { Id = 3, Name = "Omar", Salary = 8000 },
+            // new Employee { Id = 4, Name = "Ali", Salary = 7000 },
+            // new Employee { Id = 5, Name = "Mostafa", Salary = 10000 }
+            // };
+
+            // Func<Employee, Employee, bool> compareByNameLength =
+            //(a, b) => a.Name.Length > b.Name.Length;
+
+            // SortingAlgorithm5.Sort(employees, compareByNameLength);
+
+            // foreach (Employee employee in employees)
+            // {
+            //     Console.WriteLine(employee);
+            // } 
+            #endregion
+
+            #region Problem 6
+            //int[] numbers = { 5, 2, 9, 1, 6 };
+
+            //SortingAlgorithm5.Sort(numbers, (a, b) => a < b);
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //} 
+            #endregion
+
+            #region Problem 7
+            //int[] numbers = { 5, 2, 8, 1, 9, 7 };
+
+            //Console.WriteLine("Before Swap:");
+            //Console.WriteLine(string.Join(", ", numbers));
+
+            //SortingAlgorithm7.Swap(ref numbers[0], ref numbers[4]);
+
+            //Console.WriteLine("\nAfter Swapping first and last elements:");
+            //Console.WriteLine(string.Join(", ", numbers));
+
+            //SortingAlgorithm7.Sort(numbers);
+
+            //Console.WriteLine("\nAfter Sorting:");
+            //Console.WriteLine(string.Join(", ", numbers)); 
+            #endregion
+
+            #region Problem 8
+            //    Employee[] employees =
+            //{
+            //    new Employee { Id = 1, Name = "Ahmed", Salary = 9000 },
+            //    new Employee { Id = 2, Name = "Mona", Salary = 12000 },
+            //    new Employee { Id = 3, Name = "Omar", Salary = 12000 },
+            //    new Employee { Id = 4, Name = "Ali", Salary = 7000 },
+            //    new Employee { Id = 5, Name = "Mostafa", Salary = 9000 }
+            //};
+
+            //    Console.WriteLine("Before Sorting:");
+            //    foreach (var e in employees)
+            //        Console.WriteLine(e);
+
+            //    SortingAlgorithm8.Sort(employees);
+
+            //    Console.WriteLine("\nAfter Sorting (by Salary, then Name):");
+            //    foreach (var e in employees)
+            //        Console.WriteLine(e); 
+            #endregion
+
+            #region Problem 9
+
+            //    Employee[] employees =
+            //{
+            //    new Employee { Id = 1, Name = "Ahmed", Salary = 9000 },
+            //    new Employee { Id = 2, Name = "Mona", Salary = 12000 },
+            //    new Employee { Id = 3, Name = "Omar", Salary = 8000 },
+            //    new Employee { Id = 4, Name = "Ali", Salary = 7000 },
+            //    new Employee { Id = 5, Name = "Mostafa", Salary = 10000 }
+            //};
+
+            //    Console.WriteLine("Original Employees:");
+            //    foreach (var emp in employees)
+            //        Console.WriteLine(emp);
+
+            //    Console.WriteLine("\nSorted Employees (cloned):");
+            //    Employee[] sorted = SortingAlgorithm<Employee>.Sort(employees);
+
+            //    foreach (var emp in sorted)
+            //        Console.WriteLine(emp);
+
+            //    Console.WriteLine("\nAfter Sorting, Original still unchanged:");
+            //    foreach (var emp in employees)
+            //        Console.WriteLine(emp);
+
+
+            #endregion
+
+            #region Problem 10
+            //List<string> names = new List<string> { "mahmoud", "ahmed", "sara" };
+
+            //var upperNames = Transformation.ApplyTransformation(names, s => s.ToUpper());
+            //Console.WriteLine("Uppercase: " + string.Join(", ", upperNames));
+
+
+            #endregion
+
+            #region problem 11
+            //int sum = operations.Calculate(10, 20, (a, b) => a + b);
+            //Console.WriteLine(sum);
+
+            //int add = operations.Calculate(10, 20, (a, b) => a * b);
+            //Console.WriteLine(add);
+
+            //int divid = operations.Calculate(10, 20, (a, b) => a / b);
+            //Console.WriteLine(divid); 
+            #endregion
+
+            #region Problem 12
+            //List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            //var num = TransformList.TransformListMethos(numbers, e => e.ToString());
+            //num.ForEach(Console.WriteLine);
+
+            //Console.WriteLine("===============================================");
+
+            //List<string> words = new List<string> { "apple", "banana", "cherry" };
+            //List<int> lengths = TransformList.TransformListMethos(words, w => w.Length);
+            //Console.WriteLine("\nWords to Lengths:");
+            //lengths.ForEach(Console.WriteLine); 
+            #endregion
+
+            #region Problem 13
+            //List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            //var x = TransformList.Squre(numbers, e => Math.Pow(e, 2));
+            //x.ForEach(Console.WriteLine); 
+            #endregion
+
+            #region Problemn 14
+            //List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var result = TransformList.Filter(numbers, e => e % 2 == 0);
+            //result.ForEach(e => Console.WriteLine(e)); 
+            #endregion
+
+            #region Problem 15
+            //    List<string> names = new List<string>
+            //{
+            //    "Alice", "Ahmed", "Bob", "Amira", "Charlie", "Anna", "David"
+            //};
+
+            //    var startWithA = TransformList.FilterStrings(names, s => s.StartsWith("A"));
+            //    Console.WriteLine("Strings starting with A:");
+            //    startWithA.ForEach(Console.WriteLine); 
+            #endregion
+
+            #region Problem 16
+
+            //int x = 10, y = 5;
+
+            //int sum = TransformList.PerformOperation(x, y, Add);
+            //Console.WriteLine($"Addition: {x} + {y} = {sum}"); 
+            #endregion
+
+            #region Problem 17
+            //double x = 10.0, y = 2.0;
+
+            //double division = TransformList.PerformOperation(x, y, (a, b) => b != 0 ? a / b : double.NaN);
+            //Console.WriteLine($"{x} / {y} = {division}");
+
+            //double power = TransformList.PerformOperation(x, y, (a, b) => Math.Pow(a, b));
+            //Console.WriteLine($"{x} ^ {y} = {power}"); 
+            #endregion
+
+        }
+
+        private static int Add(int arg1, int arg2)
+        {
+            return arg1 + arg2;
+        }
+    }
+}

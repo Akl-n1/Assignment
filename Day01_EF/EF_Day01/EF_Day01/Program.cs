acc87428ee3aad9213a6e50f0d94848f936a4c53
Akl-n1 <ma7mud3kl@gmail.com>
2025-09-08 02:44:17 +0300

    Solve Task1 EF

---
+++ Day01_EF/EF_Day01/EF_Day01/Program.cs
@@ -0,0 +1,14 @@
+﻿using EF_Day01.Context;
+using EF_Day01.Model;
+
+namespace EF_Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new ApplicationDbContext();
+
+        }
+    }
+}

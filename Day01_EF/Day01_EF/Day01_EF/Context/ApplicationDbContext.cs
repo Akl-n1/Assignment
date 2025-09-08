0507ffce248e77ed14776496dc664bbaa27faf07
Akl-n1 <ma7mud3kl@gmail.com>
2025-09-08 03:11:51 +0300

    Solve Task1 EF

---
+++ Day01_EF/Day01_EF/Day01_EF/Context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Day01_EF.ConfigrationClasses;
+using Day01_EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01_EF.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MAHMOUDAKL\\SQL22;Database=TaskEF;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
